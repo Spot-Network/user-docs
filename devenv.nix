2aad8ebb69be225f4c61b77f6aa2ce5d0ff9e016
pedro <pedro@noxus.ai>
2025-02-17 10:49:27 +0000

    chore: nix devenv

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  languages.javascript = {
+    npm.enable=true;
+  };
+}
